fd78bae9c9b8d059a5252e0eab32637f57e928e8
dbstnqls6953@naver.com <dbstnqls6953@naver.com>
2022-01-24 06:04:10 +0900

    .

--- js/app_todo.js
+++ js/app_todo.js
@@ -14,10 +14,6 @@ function setTodos(newTodos){
   todos = newTodos;
 }
 
-function getAllTodos(){
-  return todos;
-}
-
 function saveTodos(){
   localStorage.setItem("todos_key", JSON.stringify(todos));
 }
@@ -28,13 +24,17 @@ function appendTodos(text){
     id: newId,
     isCompleted: false,
     content: text});
+
   setTodos(newTodos);
   saveTodos();
+  setLeftItems();
+  checkIsAllCompleted();
   paintTodo();
 }
 
 function updateTodo(text, todoId){
   const newTodos = getAllTodos().map(item => item.id === todoId ? ({...item, content: text}): item);
+
   setTodos(newTodos);
   paintTodo();
 }
@@ -45,61 +45,172 @@ function onDbclickTodo(e, todoId){
   const todoItemElem = todoElem.parentElement; // li
   const editInput = document.createElement("input");
   editInput.classList.add("edit-input");
-
   editInput.addEventListener("keypress", (e) => {
     if(e.key === "Enter"){
       updateTodo(e.target.value, todoId);
-      // document.body.removeEventListener("click", onClickBody);
+      document.body.removeEventListener("click", onClickBody);
     }
   })
+
+  function onClickBody(e){
+    if(e.target !== editInput){
+      todoItemElem.removeChild(editInput);
+      document.body.removeEventListener("click", onClickBody);
+    }
+  }
+
+  document.body.addEventListener("click", onClickBody);
   todoItemElem.appendChild(editInput);
 }
 
-function deleteTodo(todoId){
-  const newTodos = getAllTodos().filter(item => item.id !== todoId);
+let isAllCompleted = false;
+const setIsAllCompleted = (bool) => { isAllCompleted = bool };
+
+function completeAll(){
+  completeAllBtn.classList.add("checked");
+  const newTodos = getAllTodos().map( item => ({...item, isCompleted: true}) );
+
+  setTodos(newTodos);
+}
+
+function incompleteAll(){
+  completeAllBtn.classList.remove("checked");
+  const newTodos = getAllTodos().map( item => ({...item, isCompleted: false}) );
+
+  setTodos(newTodos);
+}
+
+function onClickCompleteAll(){
+  if(!getAllTodos().length) return;
+
+  if(isAllCompleted) incompleteAll();
+  else completeAll();
+  setIsAllCompleted(!isAllCompleted);
+
+  setLeftItems();
+  paintTodo();
+}
+
+function checkIsAllCompleted(){
+  if(getAllTodos().length === getCompleteTodos().length){
+    setIsAllCompleted(true);
+    completeAllBtn.classList.add("checked");
+  } else {
+    setIsAllCompleted(false);
+    completeAllBtn.classList.remove("checked");
+  }
+}
+
+function setLeftItems(){
+  const leftTodos = getActiveTodos();
+  leftItems.innerHTML = `${leftTodos.length} items left`;
+}
+
+let currentShowType = 'all';
+function setCurrentShowType(newShowType){
+  currentShowType = newShowType;
+} 
+
+function onClickShowTodosType(e){
+  const currentBtn = e.target;
+  const newShowType = currentBtn.dataset.type;
+
+  if(currentShowType === newShowType) return;
+
+  const preBtn = document.querySelector(`#show-${currentShowType}-btn`);
+  preBtn.classList.remove("selected");
+  
+  currentBtn.classList.add("selected");
+  setCurrentShowType(newShowType);
+
+  paintTodo();
+}
+
+function onClickclearCompleted(){
+  const newTodos = getActiveTodos();
+
   setTodos(newTodos);
   paintTodo();
+}
+
+function getAllTodos(){
+  return todos;
+}
+
+ function getActiveTodos(){
+  return todos.filter(item => item.isCompleted === false);
+ }
+
+function getCompleteTodos(){
+  return todos.filter(item => item.isCompleted === true);
 }
 
 function completeTodo(todoId){
-  const newTodos = getAllTodos().map(item => item.id === todoId ? ({...item, isCompleted: !item.isCompleted}) : item);
+  const newTodos = getAllTodos().map( item => item.id === todoId ? ({...item, isCompleted: !item.isCompleted}) : item );
+
   setTodos(newTodos);
+  paintTodo();
+  setLeftItems();
+  checkIsAllCompleted();
+}
+
+function deleteTodo(todoId){
+  const newTodos = getAllTodos().filter( item => item.id !== todoId );
+
+  setTodos(newTodos);
+  setLeftItems();
   paintTodo();
 }
 
-function paintTodo(item){
+function paintTodo(){
   toDoList.innerHTML = null;
-  const allTodos = getAllTodos();
+  
+  switch(currentShowType){
+    case 'all':
+      const allTodos = getAllTodos();
+      allTodos.forEach(item => { paintTodos(item); });
+      break;
+    case 'active':
+      const activeTodos = getActiveTodos();
+      activeTodos.forEach(item => { paintTodos(item); });
+      break;
+    case 'completed':
+      const completedTodos = getCompleteTodos();
+      completedTodos.forEach(item => { paintTodos(item); });
+      break;
+    default:
+      break;
+  }
+}
 
-  allTodos.forEach(item => {
-    const toDoItem = document.createElement("li");
-    toDoItem.classList.add("todo-item");
+function paintTodos(item){
+  const toDoItem = document.createElement("li");
+  toDoItem.classList.add("todo-item");
 
-    const checkBox = document.createElement("div");
-    checkBox.classList.add("checkbox");
-    checkBox.addEventListener("click", () => completeTodo(item.id));
+  const checkBox = document.createElement("div");
+  checkBox.classList.add("checkbox");
+  checkBox.addEventListener("click", () => completeTodo(item.id));
 
-    const toDo = document.createElement("div");
-    toDo.classList.add("todo");
-    toDo.innerText = item.content;
-    toDo.addEventListener("dblclick", (e) => onDbclickTodo(e, item.id));
+  const toDo = document.createElement("div");
+  toDo.classList.add("todo");
+  toDo.innerText = item.content;
+  toDo.addEventListener("dblclick", (e) => onDbclickTodo(e, item.id));
 
-    const delBtn = document.createElement("button");
-    delBtn.classList.add("delBtn");
-    delBtn.innerHTML = "X";
-    delBtn.addEventListener("click", () => deleteTodo(item.id));
+  const delBtn = document.createElement("button");
+  delBtn.classList.add("delBtn");
+  delBtn.innerHTML = "X";
+  delBtn.addEventListener("click", () => deleteTodo(item.id));
 
-    if(item.isCompleted){
-      toDoItem.classList.add("checked");
-      checkBox.innerText = "✔";
-    }
+  if(item.isCompleted){
+    toDoItem.classList.add("checked");
+    checkBox.innerText = "✔";
+  }
 
-    toDoItem.appendChild(checkBox);
-    toDoItem.appendChild(toDo);
-    toDoItem.appendChild(delBtn);
+  toDoItem.appendChild(checkBox);
+  toDoItem.appendChild(toDo);
+  toDoItem.appendChild(delBtn);
 
-    toDoList.appendChild(toDoItem);
-  })
+  toDoList.appendChild(toDoItem);
 }
 
 function init(){
@@ -109,7 +220,14 @@ function init(){
       toDoInput.value = "";
     }
   })
-  saveTodos();
+
+  showAllBtn.addEventListener("click", onClickShowTodosType);
+  showActvieBtn.addEventListener("click", onClickShowTodosType);
+  showCompletedBtn.addEventListener("click", onClickShowTodosType);
+  clearCompletedBtn.addEventListener("click", onClickclearCompleted);
+  completeAllBtn.addEventListener("click", onClickCompleteAll);
+
+  setLeftItems();
 }
 
 init();
